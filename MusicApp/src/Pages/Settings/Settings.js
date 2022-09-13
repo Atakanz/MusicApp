6a2867970b548838c745b1ce7162171ef199eda3
Atakan Zerafet <62819685+Atakanz@users.noreply.github.com>
2022-09-14 01:55:29 +0300

    Delete MusicApp directory

--- MusicApp/src/Pages/Settings/Settings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-
-const Settings = () => (
-	<SafeAreaView>
-		<Text>Hello Settings!</Text>
-	</SafeAreaView>
-);
-export default Settings;
