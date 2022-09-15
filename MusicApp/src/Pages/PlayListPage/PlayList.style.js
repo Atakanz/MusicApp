c3e7051dcf0257d3241386fd210061a04cef62fd
Atakanz <atakanzer@gmail.com>
2022-09-15 21:17:45 +0300

    Play list page added.

---
+++ MusicApp/src/Pages/PlayListPage/PlayList.style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    songListView: {
+        borderColor: '#20b2aa',
+        borderTopWidth: 6,
+        borderLeftWidth: 5,
+        borderRightWidth: 5,
+        padding: 10,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        height: '90%'
+    }
+})
