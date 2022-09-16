6bbc4645e04f5b4da0e57f7e98f52231c33b5a47
Atakanz <atakanzer@gmail.com>
2022-09-16 22:17:20 +0300

    Like and unlike reducers added.

--- MusicApp/src/Management/Features/SongList/songListSlice.js
+++ MusicApp/src/Management/Features/SongList/songListSlice.js
@@ -1,19 +1,34 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  songList: [],
+  GenreList: [],
+  PlayList: [],
+  LikedSong: [],
 };
 
 export const songListSlice = createSlice({
   name: 'songList',
   initialState,
   reducers: {
+    setGenreList: (state, action) => {
+      state.GenreList = action.payload;
+    },
+    setPlayList: (state, action) => {
+      state.PlayList = action.payload;
+    },
+    setLikedSong: (state, action) => {
+      state.LikedSong = [...state.LikedSong, action.payload];
+    },
     setSongList: (state, action) => {
-      state.songList = action.payload;
+      state.songList= action.payload;
+    },
+    unlikeSong: (state, action) => {
+      state.LikedSong = state.LikedSong.filter((item) =>
+      item.idValue !== action.payload);
     },
   },
 });
 
-export const {setSongList, songList} = songListSlice.actions;
+export const {setGenreList, setPlayList, setLikedSong, setSongList, unlikeSong} = songListSlice.actions;
 
 export default songListSlice.reducer;
