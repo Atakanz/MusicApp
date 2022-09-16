5bb271f9d600ca2af181276e18c7ad3edc098fc0
Atakanz <atakanzer@gmail.com>
2022-09-16 15:41:43 +0300

    Tracklist pages added.

---
+++ MusicApp/src/Pages/TrackListGenre/TrackListGenre.js
@@ -0,0 +1,48 @@
+import React, {useEffect} from "react";
+import {SafeAreaView, View, FlatList} from "react-native";
+import styles from './TrackListGenre.style';
+import PlayListUnit from "../../Components/PlayListUnit";
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
+import {useDispatch, useSelector} from "react-redux";
+import PlayListTop from "../../Components/PlayListTop";
+import useFetch from "../../Components/Hooks/useFetch";
+import {setGenreList} from "../../Management/Features/SongList/songListSlice";
+
+const TrackListGenre = ({route, navigation}) => {
+    const songIcon = <Icon name="music-note-eighth" size={40} 
+    color="darkgray" />
+    const {item} = route.params;
+    const dispatch = useDispatch();
+
+      const {response} = useFetch({
+        url: `https://api.napster.com/v2.2/genres/${item.id}/tracks/top?apikey=NjgyZGQzNzUtZWU5Mi00ZGU1LWE4ZGItYjZmMWRiYjcwY2Jm&limit=15`
+      });
+    
+    useEffect(() => {
+      if (response !== null) {
+        dispatch(setGenreList(response.tracks))
+      }
+      }, [response]);
+
+    
+    const songList = useSelector(state => state.songList.GenreList)
+    return (
+        <SafeAreaView>
+            <PlayListTop
+            name={item.name} />
+            <View style={styles.songListView} >
+            <FlatList
+            data={songList}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item}) =>
+             <PlayListUnit
+             icon={songIcon}
+             trackName={item.name}
+             artistName={item.artistName} />}
+            />
+            </View>
+        </SafeAreaView> 
+    )
+}
+
+export default TrackListGenre;
--- MusicApp/src/Pages/TrackListPlaylist/TrackListPlaylist.js
+++ MusicApp/src/Pages/TrackListPlaylist/TrackListPlaylist.js
@@ -1,40 +1,43 @@
 import React, {useEffect, useState} from "react";
 import {SafeAreaView, View, Text, FlatList, Image} from "react-native";
-import styles from './PlayList.style';
+import styles from './TrackListPlayList.style';
 import PlayListUnit from "../../Components/PlayListUnit";
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import {useDispatch, useSelector} from "react-redux";
 import PlayListTop from "../../Components/PlayListTop";
-import {setSongList} from "../../Management/Features/SongList/songListSlice";
+import useFetch from "../../Components/Hooks/useFetch";
+import {setPlayList} from "../../Management/Features/SongList/songListSlice";
 import axios from "axios";
 
-const PlayList = ({route, navigation}) => {
+const TrackListPlaylist = ({route, navigation}) => {
     const songIcon = <Icon name="music-note-eighth" size={40} 
     color="darkgray" />
-    const {id, name, images, description} = route.params;
-    const [songsInPlayList, setSongsInPlayList] = useState([])
+    const {item} = route.params;
     const dispatch = useDispatch();
-    useEffect(() => {
-         axios.get(
-        `https://api.napster.com/v2.2/playlists/${id}/tracks?apikey=NjgyZGQzNzUtZWU5Mi00ZGU1LWE4ZGItYjZmMWRiYjcwY2Jm&limit=25`
-          )
-          .then(response => {
-            setSongsInPlayList(response.data.tracks);
-          });
-      }, []);
+
+
+      const {response} = useFetch({
+        url: `https://api.napster.com/v2.2/playlists/${item.id}/tracks?apikey=NjgyZGQzNzUtZWU5Mi00ZGU1LWE4ZGItYjZmMWRiYjcwY2Jm&limit=15`
+      });
 
     
-    // console.log(url);
-    const songList = useSelector(state => state.songList.songList)
+    useEffect(() => {
+      if (response !== null) {
+       dispatch(setPlayList(response.tracks))
+      }
+      }, [response]);
+    const {images} = item;
+    const songList = useSelector(state => state.songList.PlayList)
     return (
         <SafeAreaView>
             <PlayListTop
             imageUrl={images[0].url}
-            name={name}
-            description={description} />
+            name={item.name}
+            description={item.description} />
             <View style={styles.songListView} >
             <FlatList
-            data={songsInPlayList}
+            style={styles.songList}
+            data={songList}
             showsVerticalScrollIndicator={false}
             renderItem={({item}) =>
              <PlayListUnit
@@ -47,4 +50,4 @@ const PlayList = ({route, navigation}) => {
     )
 }
 
-export default PlayList;
+export default TrackListPlaylist;
