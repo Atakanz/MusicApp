528064623422d6ef280987bb6c6cc942c8d38cde
Atakanz <atakanzer@gmail.com>
2022-09-16 22:16:35 +0300

    Header right icon added.

--- MusicApp/src/Navigation/BottomTab.js
+++ MusicApp/src/Navigation/BottomTab.js
@@ -3,13 +3,20 @@ import React from 'react';
 import HomeStack from './HomeStack';
 import GenreStack from './GenreStack';
 import ProfileStack from './ProfileStack';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
+import {Pressable} from 'react-native';
 
 const Tab = createBottomTabNavigator();
+const editProfile = <Icon name="pencil-outline" size={30} 
+    color="darkgray" style={{marginRight: 15}} />
 
-export const BottomTab = () => (
+export const BottomTab = ({navigation}) => (
 	<Tab.Navigator>
 		<Tab.Screen name='Home' component={HomeStack} />
 		<Tab.Screen name='Search' component={GenreStack} />
-		<Tab.Screen name='Profile' component={ProfileStack} />
+		<Tab.Screen name='Profile' component={ProfileStack} 
+		options={{headerRight: () => 
+			(<Pressable onPress={() => navigation.navigate("Settings")}>{editProfile}</Pressable>)}} 
+             />
 	</Tab.Navigator>
 );
